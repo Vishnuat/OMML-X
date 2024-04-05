802a597f08647468981d743d31201390f0de04d1
Daneil-k <158211667+Daneil-k@users.noreply.github.com>
2024-04-05 20:11:53 +0530

    Update wzml_main.py

--- bot/helper/themes/wzml_main.py
+++ bot/helper/themes/wzml_main.py
@@ -2,12 +2,11 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
+    ST_BN1_NAME = 'Owner'
+    ST_BN1_URL = 'https://t.me/TK_Mob_TY'
     ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/WZML_X'
-    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
-<b>Type {help_command} to get a list of available commands</b>'''
+    ST_BN2_URL = 'https://t.me/Tamil_Mob_HD'
+    ST_MSG = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
     OWN_TOKEN_GENERATE = '''<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</i>'''
